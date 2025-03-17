4c2c83a0446fffe459d224fc2052f0bb78248e49
Chetan Kumar <chetankumarsurra4544@gmail.com>
2025-03-17 18:29:54 +0530

    Updated authentication system and fixed UI bugs

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import home, category1, category2, category3
+from .views import home, signup_view, login_view, logout_view, profile_view, category_products
 from . import views
 urlpatterns = [
-    path('', home, name='home'),
-    path('electronics/', category1, name='category1'),
-    path('fashion/', category2, name='category2'),
-    path('accessories/', category3, name='category3'),
-     path('category/<str:category>/', views.category_products, name='category_products'),
+    path('', views.home, name='home'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('profile/', profile_view, name='profile'),
+    path('category/<str:category>/', views.category_products, name='category_products'),
 
 ]
 
--- shop/views.py
+++ shop/views.py
@@ -1,27 +1,52 @@
 from django.shortcuts import render, redirect
 from .models import Product
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm
 
 # Create your views here.
 
 def home(request):
-    products = Product.objects.all()
-    return render(request, 'shop/home.html', {'products': products})
-
-def category1(request):
-    return render(request, 'shop/category1.html')
-
-def category2(request):
-    return render(request, 'shop/category2.html')
-
-def category3(request):
-    return render(request, 'shop/category3.html')
+    # products = Product.objects.all()
+    # return render(request, 'shop/home.html', {'products': products})
+    return render(request, 'shop/home.html')
 
 def category_products(request, category):
     products = Product.objects.filter(category=category)  # Filter products by category
     return render(request, 'shop/category.html', {'products': products, 'category': category})
 
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'shop/signup.html', {'form': form})
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'shop/login.html', {'error': 'Invalid Credentials'})
+        
+    return render(request, 'shop/login.html')
+
+@login_required
+def profile_view(request):
+    return render(request, 'shop/profile.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+ 
 
 
 
